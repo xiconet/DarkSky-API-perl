e7919fd6d3b0baaababc3945774b1d8d86cf45a5
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2013-02-12 16:24:47 -0500

    bump version

--- lib/DarkSky/API.pm
+++ lib/DarkSky/API.pm
@@ -14,11 +14,11 @@ the United States.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 our $DARKSKY_API_URL = 'https://api.darkskyapp.com/v1';
 
